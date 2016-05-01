8a8ce4dad764b217140ffd568329fcc9123bb25c
Arthurisme <is@live.ca>
2016-05-01 15:18:55 -0400

    passing parameters use get

--- first-web-application/src/main/java/com/in28minutes/LoginServlet.java
+++ first-web-application/src/main/java/com/in28minutes/LoginServlet.java
@@ -1,6 +1,7 @@
 package com.in28minutes;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,8 +13,28 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(urlPatterns= "/login.do")
 public class LoginServlet extends HttpServlet {
 	
+	 @Override
 	  protected void doGet(HttpServletRequest request, 
 			  HttpServletResponse response) throws ServletException, IOException {
+		 
+		 //11,get params:
+		 String name = request.getParameter("name");
+		 System.out.println(name);
+		 request.setAttribute("name", name);
+		 //2,jsp:
+		 request.getRequestDispatcher("/WEB-INF/views/login.jsp").forward(request, response);
+		 
+		 
+		 //1, non jsp:
+//		PrintWriter writer = response.getWriter();
+//        writer.println("<html>");
+//        writer.println("<head>");
+//        writer.println("<title>Yahoo!!!!!!!!</title>");
+//        writer.println("</head>");
+//        writer.println("<body>");
+//        writer.println("My First Servlet");
+//        writer.println("</body>");
+//        writer.println("</html>");
 	}
 
 
